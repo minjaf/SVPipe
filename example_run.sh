f619b87eae0ba18d010f855b96994d29144be232
minja <zakazserov@gmail.com>
2023-07-26 18:59:32 +0300

    add example run file

---
+++ example_run.sh
@@ -0,0 +1,2 @@
+for i in {101..101}; do for j in ~/genomes/crams/${i}*.cram; do bash create_data_and_configs.sh $j /home/vsfishman/genomes/process/i$i/; done; done;
+bash run_cue.sh /home/vsfishman/genomes/process/i101/
